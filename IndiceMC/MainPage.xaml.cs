aaabd0f69006e14d74a1b1a9e73e41eef69119fc
Christian Lugo <christian86360@gmail.com>
2021-04-02 21:54:09 -0600

    Agregar archivos de proyecto.

---
+++ IndiceMC/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace IndiceMC
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalcular_Clicked(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrEmpty(txtAltura.Text) && !string.IsNullOrEmpty(txtPeso.Text))
+            {
+                CalcularIMC();
+            }
+            else
+            {
+                DisplayAlert("Datos vacios", "Llene los campos vacios", "Ok");
+            }
+           
+        }
+        private void CalcularIMC()
+        {
+            double altura = Convert.ToDouble(txtAltura.Text);
+            double peso = Convert.ToDouble(txtPeso.Text);
+            double resultado = peso / (altura * altura);
+            txtResultado.Text = resultado.ToString();
+            string mensaje = "";
+            if (resultado < 18.5)
+            {
+                mensaje = "Tienes bajo peso";
+            }
+            else if (resultado >= 18.5 && resultado <= 24.9)
+            {
+                mensaje = "Tu peso es normal";
+            }
+            else if (resultado >= 25 && resultado < 29.9)
+            {
+                mensaje = "Tienes sobrepeso";
+            }
+            else
+            {
+                mensaje = "Tienes sobrepeso";
+            }
+
+            DisplayAlert($"Resultado:{resultado}", mensaje, "Ok");
+        }
+    }
+}
